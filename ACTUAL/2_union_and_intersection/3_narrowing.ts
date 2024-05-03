0433ab99a2291023da79a9b87cd52fefe5c31803
Oster <kms0843@naver.com>
2024-05-04 00:03:53 +0900

    narrowing

--- ACTUAL/2_union_and_intersection/3_narrowing.ts
+++ ACTUAL/2_union_and_intersection/3_narrowing.ts
@@ -7,5 +7,20 @@
 let numberOrString: number | string = 'code Factory';
 numberOrString;
 
-const decimal = 12.3278;
-console.log(decimal.toFixed(2));
+// const decimal = 12.3278;
+// console.log(decimal.toFixed(2));
+
+// numberOrString.toFixed();
+
+/**
+ * Narrowing 종류
+ *
+ * 1) Assignment Narrowing
+ * 2) typeof Narrowing
+ * 3) Truthiness Narrowing
+ * 4)Equality Narrowing
+ * 5) in operator narrowing
+ * 6) instanceof Narrowing
+ * 7) discrimated union narrowing (차별된 유니언 내로잉)
+ * 8) exhaustiveness checking
+ */
