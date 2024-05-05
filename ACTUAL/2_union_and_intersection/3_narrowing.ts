12721621d55f3718b00895434ad0cfc5a9d56343
Oster <kms0843@naver.com>
2024-05-05 22:41:46 +0900

    내로잉

--- ACTUAL/2_union_and_intersection/3_narrowing.ts
+++ ACTUAL/2_union_and_intersection/3_narrowing.ts
@@ -4,8 +4,8 @@
  * Narrowing은 Union 타입에서 더욱 구체적인 타입으로
  * 논리적으로 유추 할 수 있게 되는걸 의미한다.
  */
-let numberOrString: number | string = 'code Factory';
-numberOrString;
+// let numberOrString: number | string = 'code Factory';
+// numberOrString;
 
 // const decimal = 12.3278;
 // console.log(decimal.toFixed(2));
@@ -24,3 +24,10 @@ numberOrString;
  * 7) discrimated union narrowing (차별된 유니언 내로잉)
  * 8) exhaustiveness checking
  */
+
+// (1) Assignment Narrowing
+// 특정 값을 할당해서 내로잉
+
+let numbOrString: number | string = '아이유';
+
+numbOrString.toString();
